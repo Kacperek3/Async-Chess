77f47c95a984af76d187c0cb47ae24f3a64e95af
Kacper Sztuka <kapisztuka1@gmail.com>
2026-01-19 13:03:34 +0100

    feature: build_and_run.sh - new shell script to running game fast

---
+++ build_and_run.sh
@@ -0,0 +1,29 @@
+BUILD_DIR="build"
+EXE_FILE="Chess"
+COMPILE_CONFIG="$1"
+
+set -e
+echo "----------------"
+echo "Start building"
+
+if [ "$COMPILE_CONFIG" = "Release" ]; then 
+    echo "Config for Release mode "
+    cmake -S . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Release 
+else
+    echo "Default debug mode"
+    cmake -S . -B $BUILD_DIR 
+fi
+
+echo "Compiling source" 
+cmake --build $BUILD_DIR --config $COMPILE_CONFIG --parallel 4
+
+echo "-----------------"
+echo "Running Game"
+
+if [ -f "$BUILD_DIR/$EXE_FILE" ]; then
+    cd $BUILD_DIR
+    ./$EXE_FILE 
+else
+    echo "didn't find executable file"
+    exit 1
+fi
